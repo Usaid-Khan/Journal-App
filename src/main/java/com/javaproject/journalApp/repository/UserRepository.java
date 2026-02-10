a8c97674362475e47e554cc3148b4afcb9553b1e
Usaid-Khan <usaid1521@gmail.com>
2026-02-10 13:57:28 +0500

    Enabling Spring Security and Securing User Endpoints

--- src/main/java/com/javaproject/journalApp/repository/UserRepository.java
+++ src/main/java/com/javaproject/journalApp/repository/UserRepository.java
@@ -9,4 +9,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends MongoRepository<User, ObjectId> {
     User findByUserName(String username);
+
+    void deleteByUserName(String userName);
 }
