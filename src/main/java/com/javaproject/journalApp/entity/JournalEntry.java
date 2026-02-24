3712180b5ecafbea6782a82d319ec27fc1425978
Usaid-Khan <usaid1521@gmail.com>
2026-02-24 08:12:44 +0500

    Enhancing sentiment logic

--- src/main/java/com/javaproject/journalApp/entity/JournalEntry.java
+++ src/main/java/com/javaproject/journalApp/entity/JournalEntry.java
@@ -1,5 +1,6 @@
 package com.javaproject.journalApp.entity;
 
+import com.javaproject.journalApp.enums.Sentiment;
 import lombok.*;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
@@ -17,5 +18,6 @@ public class JournalEntry {
     @NonNull
     private String title;
     private String content;
+    private Sentiment sentiment;
     private LocalDateTime date;
 }
--- src/main/java/com/javaproject/journalApp/scheduler/UserScheduler.java
+++ src/main/java/com/javaproject/journalApp/scheduler/UserScheduler.java
@@ -3,16 +3,18 @@ package com.javaproject.journalApp.scheduler;
 import com.javaproject.journalApp.cache.AppCache;
 import com.javaproject.journalApp.entity.JournalEntry;
 import com.javaproject.journalApp.entity.User;
+import com.javaproject.journalApp.enums.Sentiment;
 import com.javaproject.journalApp.repository.UserRepositoryImpl;
 import com.javaproject.journalApp.service.EmailService;
-import com.javaproject.journalApp.service.SentimentAnalysisService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @Component
@@ -22,8 +24,6 @@ public class UserScheduler {
     @Autowired
     private EmailService emailService;
     @Autowired
-    private SentimentAnalysisService sentimentAnalysisService;
-    @Autowired
     private AppCache appCache;
 
     @Scheduled(cron = "0 0 9 * * SUN")
@@ -32,11 +32,27 @@ public class UserScheduler {
 
         for(User user : users) {
             List<JournalEntry> journalEntries = user.getJournalEntries();
-            List<String> filteredEntriesData = journalEntries.stream().filter(x -> x.getDate().isAfter(LocalDateTime.now().minus(7, ChronoUnit.DAYS))).map(x -> x.getContent()).collect(Collectors.toList());
-            String entry = String.join(" ", filteredEntriesData);
-            String sentiment = sentimentAnalysisService.getSentiment(entry);
+            List<Sentiment> sentiments = journalEntries.stream().filter(x -> x.getDate().isAfter(LocalDateTime.now().minus(7, ChronoUnit.DAYS))).map(x -> x.getSentiment()).collect(Collectors.toList());
+            Map<Sentiment, Integer> sentimentCounts = new HashMap<>();
 
-            emailService.sendEmail(user.getEmail(), "Sentiment for last 7 days", sentiment);
+            for(Sentiment sentiment : sentiments) {
+                if(sentiment != null) {
+                    sentimentCounts.put(sentiment, sentimentCounts.getOrDefault(sentiment, 0) + 1);
+                }
+            }
+
+            Sentiment mostFrequentSentiment = null;
+            int maxCount = 0;
+            for(Map.Entry<Sentiment, Integer> entry : sentimentCounts.entrySet()) {
+                if(entry.getValue() > maxCount) {
+                    maxCount = entry.getValue();
+                    mostFrequentSentiment = entry.getKey();
+                }
+            }
+
+            if(mostFrequentSentiment != null) {
+                emailService.sendEmail(user.getEmail(), "Sentiment for last 7 days", mostFrequentSentiment.toString());
+            }
         }
     }
 
