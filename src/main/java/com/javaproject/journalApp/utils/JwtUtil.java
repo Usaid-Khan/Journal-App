8c645d6bd9fcb2eedec28d34b9afd454e02b79b7
Usaid-Khan <usaid1521@gmail.com>
2026-03-25 02:30:18 +0500

    Adds Kafka Fallback

--- src/main/java/com/javaproject/journalApp/utils/JwtUtil.java
+++ src/main/java/com/javaproject/journalApp/utils/JwtUtil.java
@@ -30,7 +30,7 @@ public class JwtUtil {
                 .header().empty().add("typ", "JWT")
                 .and()
                 .issuedAt(new Date(System.currentTimeMillis()))
-                .expiration(new Date(System.currentTimeMillis() + 1000 * 60))
+                .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60))
                 .signWith(getSigningKey())
                 .compact();
     }
